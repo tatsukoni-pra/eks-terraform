3cc5b58b9f6897d4c74a1a1dbdf242b6a43068e5
小西達大 <konishitatsuhiro@konishitoorudainoMacBook-Pro.local>
2025-03-14 02:15:28 +0900

    second

--- eks.tf
+++ eks.tf
@@ -16,3 +16,30 @@ resource "aws_eks_cluster" "eks_test_cluster" {
     endpoint_public_access  = true
   }
 }
+
+resource "aws_eks_node_group" "node_group" {
+  cluster_name    = aws_eks_cluster.eks_test_cluster.name
+  node_group_name = "eks-test-ng"
+  node_role_arn   = aws_iam_role.node_role.arn
+  subnet_ids      = [
+    "subnet-0fc6ea4c93a919961", # tatsukoni-demo-subnet-private-1a
+    "subnet-0cdf0dfdbaff1ff9e"  #tatsukoni-demo-subnet-private-1c
+  ]
+  capacity_type   = "SPOT"
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
+  }
+  instance_types = [
+    "t3.small"
+  ]
+  # https://aws.amazon.com/jp/bottlerocket/?amazon-bottlerocket-whats-new.sort-by=item.additionalFields.postDateTime&amazon-bottlerocket-whats-new.sort-order=desc
+  ami_type = "BOTTLEROCKET_x86_64"
+
+  update_config {
+    # Desired max number of unavailable worker nodes during node group update
+    max_unavailable = 1
+  }
+}
--- iam.tf
+++ iam.tf
@@ -34,3 +34,48 @@ resource "aws_iam_role_policy_attachment" "eks_test_cluster_role_AmazonEKSCluste
 #   role       = aws_iam_role.eks_test_cluster_role.name
 #   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
 # }
+
+######
+# Node Role
+######
+resource "aws_iam_role" "node_role" {
+  name = "eks-test-node-role"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "sts:AssumeRole"
+            ],
+            "Principal": {
+                "Service": [
+                    "ec2.amazonaws.com"
+                ]
+            }
+        }
+    ]
+}
+EOF
+
+  tags = {
+    Name = "eks-test-node-role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "node_role_AmazonEKSWorkerNodePolicy" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "node_role_AmazonEKS_CNI_Policy" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "node_role_AmazonEC2ContainerRegistryReadOnly" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
