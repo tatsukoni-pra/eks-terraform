c821b9fb841e16d5f7b563d690d3b148825d3d9b
小西達大 <konishitatsuhiro@konishitoorudainoMacBook-Pro.local>
2025-06-20 13:55:59 +0900

    feat

--- eks.tf
+++ eks.tf
@@ -17,37 +17,6 @@ resource "aws_eks_cluster" "eks_test_cluster" {
   }
 }
 
-resource "aws_eks_node_group" "node_group" {
-  cluster_name    = aws_eks_cluster.eks_test_cluster.name
-  node_group_name = "eks-test-ng"
-  node_role_arn   = aws_iam_role.node_role.arn
-  subnet_ids = [
-    "subnet-0fc6ea4c93a919961", # tatsukoni-demo-subnet-private-1a
-    "subnet-0cdf0dfdbaff1ff9e"  # tatsukoni-demo-subnet-private-1c
-  ]
-  capacity_type = "SPOT"
-  launch_template {
-    id      = aws_launch_template.node_group.id
-    version = aws_launch_template.node_group.latest_version
-  }
-
-  scaling_config {
-    desired_size = 1
-    max_size     = 2
-    min_size     = 1
-  }
-  instance_types = [
-    "m7a.medium"
-  ]
-  # https://aws.amazon.com/jp/bottlerocket/?amazon-bottlerocket-whats-new.sort-by=item.additionalFields.postDateTime&amazon-bottlerocket-whats-new.sort-order=desc
-  ami_type = "BOTTLEROCKET_x86_64"
-
-  update_config {
-    # Desired max number of unavailable worker nodes during node group update
-    max_unavailable = 1
-  }
-}
-
 resource "aws_eks_node_group" "node_group_arm" {
   cluster_name    = aws_eks_cluster.eks_test_cluster.name
   node_group_name = "eks-test-ng-arm"
